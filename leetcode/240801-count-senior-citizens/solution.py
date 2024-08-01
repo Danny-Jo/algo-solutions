908767e77883104f97561eac44cb037622da4872
Danny-Jo <dannyjo.world@gmail.com>
2024-08-01 22:10:47 +0900

    feat(leetcode): Number of Senior Citizens

---
+++ leetcode/240801-count-senior-citizens/solution.py
@@ -0,0 +1,7 @@
+def countSeniors(details: List[str]) -> int:
+        count = 0
+        for detail in details:
+            age = int(detail[11:13])
+            if age > 60:
+                count += 1
+        return count
