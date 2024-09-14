16e9844d0728054dd7be305fe2b31c632818c9b2
Danny-Jo <dannyjo.world@gmail.com>
2024-09-14 23:48:11 +0900

    feat(leetcode): Longest Subarray With Maximum Bitwise AND

---
+++ leetcode/240914-longest-subarray-with-maximum-bitwise-and/solution.py
@@ -0,0 +1,14 @@
+def longestSubarray(nums):
+        max_val = max(nums)
+    
+        longest = 0
+        current_length = 0
+        
+        for num in nums:
+            if num == max_val:
+                current_length += 1
+                longest = max(longest, current_length)
+            else:
+                current_length = 0
+                
+        return longest
